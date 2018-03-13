cbb06fadaf2bb5b3bd47ec60e50fa32d298caf08
Adam Argyle <atom@argyleink.com>
2018-03-12 20:56:50 -0700

    smarter css importing

---
+++ app/css/index.js
@@ -0,0 +1,4 @@
+export vars from './vars.css'
+export animation from './animation.css'
+export typography from './typography.css'
+export button from './button.css'
--- app/js/home.js
+++ app/js/home.js
@@ -1,10 +1,2 @@
 import { $ } from './utilities/shorthands'
-
-import '../css/vars.css'
-import '../css/animation.css'
-import '../css/typography.css'
-import '../css/button.css'
-
-let foo = async () => console.log('async')
-
-console.log($('body'))
+import { vars, animation, typography, button } from '../css'
