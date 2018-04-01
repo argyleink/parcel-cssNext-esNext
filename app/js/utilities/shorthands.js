787a77208eee8916c42a486a44ec776939b3f9dc
Adam Argyle <atom@argyleink.com>
2018-03-31 19:50:43 -0700

    nice dom extensions

--- app/js/utilities/shorthands.js
+++ app/js/utilities/shorthands.js
@@ -10,3 +10,19 @@ export const $$ = (query, context = document) =>
 export const setAttributes = (el, attrs) =>
   Object.keys(attrs).forEach(key =>
     el.setAttribute(key, attrs[key]))
+
+// EXTEND DOM
+// $('.foo').on('click', fn)
+Node.prototype.on = window.on = function(name, fn) {
+  this.addEventListener(name, fn)
+}
+
+// $$('.foo').on('click', fn)
+// NOTE: try to avoid this, listen once on a parent instead of for n children
+NodeList.prototype.on = NodeList.prototype.addEventListener = function(name, fn) {
+  this.forEach((elem, i) =>
+    elem.on(name, fn))
+}
+
+// $$('.foo').map(el => ...)
+NodeList.prototype.__proto__ = Array.prototype
