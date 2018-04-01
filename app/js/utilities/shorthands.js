efe3993ce95cdfb54be64b1a96b9e1fff2bf8594
Adam Argyle <atom@argyleink.com>
2018-03-31 22:01:14 -0700

    no longer mutates all dom

--- app/js/utilities/shorthands.js
+++ app/js/utilities/shorthands.js
@@ -1,32 +1,28 @@
-// short querySelector: $('.foo') $('.child', context_node)
-export const $ = (query, context = document) =>
-  context.querySelector(query)
-
-// shorthand querySelectorAll: $$('.foo') $$('.child', context_node)
-export const $$ = (query, context = document) =>
-  context.querySelectorAll(query)
-
-// set multiple attributes on a node
-export const setAttributes = (el, attrs) =>
-  Object.entries(attrs)
-    .forEach(([key, val]) =>
-      el.setAttribute(key, val))
-
-// EXTEND DOM
-// $('.foo').on('click', fn)
-Node.prototype.on = window.on = function(names, fn) {
+// node decorators
+const nodeOnEvent = function(names, fn) {
   names
     .split(' ')
     .forEach(name =>
       this.addEventListener(name, fn))
 }
 
-// $$('.foo').on('click', fn)
-// NOTE: try to avoid this, listen once on a parent instead of for n children
-NodeList.prototype.on = NodeList.prototype.addEventListener = function(names, fn) {
-  this.forEach((elem, i) =>
-    elem.on(names, fn))
+const nodesOnEvent = function(names, fn) {
+  this
+    .map($el => Object.assign($el, {on:nodeOnEvent}))
+    .forEach(($el, i) =>
+      $el.on(names, fn))
 }
 
-// $$('.foo').map(el => ...)
-NodeList.prototype.__proto__ = Array.prototype
+// short querySelector: $('.foo') $('.child', context_node)
+export const $ = (query, $context = document) =>
+  Object.assign($context.querySelector(query), {on:nodeOnEvent})
+
+// shorthand querySelectorAll: $$('.foo') $$('.child', context_node)
+export const $$ = (query, $context = document) =>
+  Object.assign([...$context.querySelectorAll(query)], {on:nodesOnEvent})
+
+// set multiple attributes on a node
+export const setAttributes = (el, attrs) =>
+  Object.entries(attrs)
+    .forEach(([key, val]) =>
+      el.setAttribute(key, val))
