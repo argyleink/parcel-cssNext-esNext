28c381b4581479366aa1fb09e077e9c6b4210f81
Adam Argyle <atom@argyleink.com>
2018-03-31 23:08:09 -0700

    even slicker shorthand, no mutations

--- app/js/utilities/shorthands.js
+++ app/js/utilities/shorthands.js
@@ -1,28 +1,32 @@
 // node decorators
-const nodeOnEvent = function(names, fn) {
-  names
-    .split(' ')
-    .forEach(name =>
-      this.addEventListener(name, fn))
-}
-
-const nodesOnEvent = function(names, fn) {
-  this
-    .map($el => Object.assign($el, {on:nodeOnEvent}))
-    .forEach(($el, i) =>
-      $el.on(names, fn))
+const sugar = {
+  on: function(names, fn) {
+    names
+      .split(' ')
+      .forEach(name =>
+        this.addEventListener(name, fn))
+  },
+  setAttributes: function(attrs) {
+    Object.entries(attrs)
+      .forEach(([key, val]) =>
+        this.setAttribute(key, val))
+  }
 }
 
 // short querySelector: $('.foo') $('.child', context_node)
 export const $ = (query, $context = document) =>
-  Object.assign($context.querySelector(query), {on:nodeOnEvent})
+  Object.assign($context.querySelector(query), sugar)
 
 // shorthand querySelectorAll: $$('.foo') $$('.child', context_node)
 export const $$ = (query, $context = document) =>
-  Object.assign([...$context.querySelectorAll(query)], {on:nodesOnEvent})
-
-// set multiple attributes on a node
-export const setAttributes = (el, attrs) =>
-  Object.entries(attrs)
-    .forEach(([key, val]) =>
-      el.setAttribute(key, val))
+  Object.assign(
+    [...$context.querySelectorAll(query)]
+    .map($el => Object.assign($el, sugar))
+  , {
+    on: function(names, fn) {
+      this.forEach($el => $el.on(names, fn))
+    },
+    setAttributes: function(attrs) {
+      this.forEach($el => $el.setAttributes(attrs))
+    }
+  })
